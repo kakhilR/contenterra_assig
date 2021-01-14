2f7366b9cb26b50c9f2227ee9500e60e670c4d18
karenae <akhilkarenae@gmail.com>
2021-01-14 18:09:27 +0530

    port assing and database config

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRouter = require('./routes/user.js')
-
+const userRouter = require('./routes/user.js');
+const env =require('dotenv')
+env.config();
 const app = express();
 
 
@@ -9,11 +10,15 @@ app.get('/', (req, res) =>{
     res.send(JSON.stringify({message:"Hello"}))
 })
 
+//database connection
 const db = require('./config/keys.js').MongoURI
 mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex:true})
 .then(()=>console.log('database connected')).catch((err)=>console.log(err))
 
+//middleware
 app.use(express.json())
+
+//routes
 app.use('/api',userRouter);
 
 
